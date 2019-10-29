ccd2383a134511d67996c8ef635f04e1bf01d4ce
Isha <lautrey.isha@gmail.com>
2019-10-29 16:10:17 +0530

    trying double checking pwd

--- student/urls.py
+++ student/urls.py
@@ -1,10 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 app='student'
 urlpatterns = [
-
+    
     path('student_login',view=views.student_login,name='student_login'),
     path('student_register',view=views.student_register,name='student_register'),
     path('student_viewing',view=views.student_viewing,name='student_viewing'),
--- student/views.py
+++ student/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
 def student_login(request):
-    return render(request,'student_login.html')
+    if request.method == 'post':
+        user = auth.authenticate(rollnumber=request.POST['rollnumber'],password=request.POST['password'])
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home.html')
+        else:
+            return render(request,'student_login.html',{'error':'Roll Number or Password incorrect.'})
+    else:
+        return render(request,'student_login.html')
+
 def student_register(request):
     return render(request,'student_register.html')
+    """if request.method == 'post':
+        # user has info and wants to create an account now.
+        if request.POST['password1']==request.POST['password2']:
+            user = User.objects.get(rollnumber=request.POST['rollnumber'])
+            return render(request, 'student_register.html', {'error':'Roll Number already exixts.'})
+    else:
+        return render(request,'student_register.html')"""
+   
+        
+    
 def student_viewing(request):
     return render(request,'student_viewing.html')
 
